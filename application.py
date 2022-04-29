a8dca9d69f03c27b5a4860cb2399119ff886529e
TucspDretcm <alexander.carpio@ucsp.edu.pe>
2022-04-29 15:25:44 -0500

    cambio total para flask

--- application.py
+++ application.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+database = []
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+
+@app.route("/new_user", methods=["POST"])
+def newUser():
+
+	database.append({"user": request.form.get("user"), 
+					"password": request.form.get("password")})
+
+	return render_template("session.html", data=data)
+
+
+@app.route("/login", methods=["POST"])
+def validate_user():
+	user = request.form.get("user")
+	password = request.form.get("password")
+
+	for db in database:
+		if db["user"] == user and db["password"]==password:
+			return redirect(url_for("pantalla_principal.html"))
+			
+	return redirect(url_for("login.html"))
+
+
+
+@app.route("/show_users")
+def show_all_data():
+	return render_template("users.html", db=database)
