f65249b2e415375e51365910b03ff5f1acfb4b2c
TucspDretcm <alexander.carpio@ucsp.edu.pe>
2022-04-29 20:30:52 -0500

    base de datos update

--- application.py
+++ application.py
@@ -5,38 +5,41 @@ app = Flask(__name__)
 
 database = []
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
-
-
-@app.route("/register", methods=["POST"])
-def newUser():
-	database.append({"user": request.form.get("user"), 
-					"password": request.form.get("password")})
-	print (database)
 	return render_template("login.html")
 
 
+@app.route("/login", methods=["POST"])
+def newUser():
+	user = request.form.get("user")
+	password = request.form.get("password")
+	password_2 = request.form.get("password_2")
+
+	if password == password_2:
+		database.append({"user": user, "password": password})
+		return render_template("login.html")
+
+	return redirect("register")   # redireciona a la ruta "../register"
+
+
 @app.route("/bienvenido", methods=["POST"])
-def validate_user():
+def validateUser():
 	user = request.form.get("user")
 	password = request.form.get("password")
 
 	for db in database:
 		if db["user"] == user and db["password"]==password:
-			#return render_template("index.html")
-			print ( "Si existe 123412132" )
+			return render_template("bienvenido.html", name=user)
 
-	return render_template("login.html")
+	return redirect("login")
+
 
 
 @app.route("/login")
-def mostrarlogin():
+def login():
 	return render_template("login.html")
-
-
-
 
 
 @app.route("/register")
@@ -44,6 +47,6 @@ def register():
 	return render_template("register.html")
 
 
-@app.route("/show_users")
+@app.route("/users")
 def show_all_data():
 	return render_template("users.html", db=database)
